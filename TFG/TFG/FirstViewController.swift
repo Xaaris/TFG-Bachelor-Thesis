b3c56d13e0ebc273b6f1194312e2d2dbaa430978
Hannes Berger <xaaris@googlemail.com>
2016-02-14 18:37:44 +0100

    Initial project setup

---
+++ TFG/TFG/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TFG
+//
+//  Created by Johannes Berger on 14.02.16.
+//  Copyright © 2016 Johannes Berger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
