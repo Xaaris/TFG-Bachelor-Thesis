9093143642575946b774ba2f1ec71cad60169aca
Hannes Berger <xaaris@googlemail.com>
2016-02-20 22:20:50 +0100

    Data read in test works

--- TFG/TFG/MainViewController.swift
+++ TFG/TFG/MainViewController.swift
@@ -20,6 +20,27 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loadExampleDataButtonPressed(sender: AnyObject) {
+        let csvURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Sample1", ofType: "csv")!)
+//                print(csvURL)
+        do{
+            let text = try String(contentsOfURL: csvURL)
+            let csv = CSwiftV(String: text, separator: ";")
+            let rows = csv.rows
+            let headers = csv.headers
+            let keyedRows = csv.keyedRows!
+//            print(headers)
+//            print(rows)
+            print("")
+            print(keyedRows[1])
+            print(keyedRows[2])
+            print(keyedRows[3])
+        }catch{
+            print(error)
+        }
+        
+
+    }
 
 }
 
