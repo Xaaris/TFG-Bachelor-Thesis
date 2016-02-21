ba4b5cdd2e91386f71bb5ec41a38ccdc1a40d98e
Hannes Berger <xaaris@googlemail.com>
2016-02-21 02:14:35 +0100

    parsed the sample doc successfully. saved data in vars and printed them to console. next step is saving them persistently

--- TFG/TFG/MainViewController.swift
+++ TFG/TFG/MainViewController.swift
@@ -9,38 +9,99 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Main View")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func loadExampleDataButtonPressed(sender: AnyObject) {
-        let csvURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Sample1", ofType: "csv")!)
-//                print(csvURL)
+        if let keyedRowsDic = loadDataFromFile("Sample1"){
+//            print(keyedRowsDic)
+            var title = ""
+            var author = ""
+            var date = ""
+            var universalTags = Set<String>()
+            for row in keyedRowsDic{
+//                print(row)
+                //Info row
+                if row["QuestionType"] == "Info"{
+                    title = row["Question"]!.isEmpty ? "No title" : row["Question"]!
+                    author = row["Hint"]!.isEmpty ? "No author" : row["Hint"]!
+                    date = row["Feedback"]!.isEmpty ? "No Date" : row["Feedback"]!
+                    for tag in row.values{
+                        if !tag.isEmpty{
+                            universalTags.insert(tag)
+                        }
+                    }
+                    universalTags.remove(title)
+                    universalTags.remove(author)
+                    universalTags.remove(date)
+                    universalTags.remove("Info")
+                //Normal row
+                }else if !row["QuestionType"]!.isEmpty {
+                    if let type = row["QuestionType"], question = row["Question"], hint = row["Hint"], feedback = row["Feedback"], correctAnswers = row["CorrectAnswers"]{
+                        var answers = Set<String>()
+                        var tags = universalTags
+                        for cell in row{
+                            if cell.0.containsString("Answer") && !cell.0.containsString("C") && !cell.1.isEmpty{
+                                answers.insert(cell.1)
+                            }else if cell.0.containsString("Tag") && !cell.1.isEmpty{
+                                tags.insert(cell.1)
+                            }
+                        }
+                        
+                        print("Type: \(type)")
+                        print("question: \(question)")
+                        print("Hint: \(hint)")
+                        print("Feedback: \(feedback)")
+                        print("CorrectAnswers: \(correctAnswers)")
+                        print("Answers: \(answers)")
+                        print("Tags: \(tags)")
+                        print("")
+                        
+                        
+                    }else{
+                        print("Unwrapping type, Question, Hint, Feedback or CorrectAnswers failed!")
+                    }
+                }
+            }
+            print("Title: \(title)")
+            print("Author: \(author)")
+            print("Date: \(date)")
+            print("Tags: \(universalTags)")
+        }else{
+            print("Could not load Data")
+        }
+        
+    }
+    
+    func loadDataFromFile(fileName: String) -> [[String : String]]? {
+        let csvURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(fileName, ofType: "csv")!)
+        //                print(csvURL)
         do{
             let text = try String(contentsOfURL: csvURL)
             let csv = CSwiftV(String: text, separator: ";")
-            let rows = csv.rows
-            let headers = csv.headers
+            //            let rows = csv.rows
+            //            let headers = csv.headers
             let keyedRows = csv.keyedRows!
-//            print(headers)
-//            print(rows)
-            print("")
-            print(keyedRows[1])
-            print(keyedRows[2])
-            print(keyedRows[3])
+            //            print(headers)
+            //            print(rows)
+            //            print("")
+            //            print(keyedRows[1])
+            //            print(keyedRows[2])
+            //            print(keyedRows[3])
+            return keyedRows
         }catch{
             print(error)
         }
-        
-
+        return nil
     }
-
+    
 }
 
