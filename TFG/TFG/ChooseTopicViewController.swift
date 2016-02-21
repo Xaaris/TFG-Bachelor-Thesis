b12d366fc834e5517042411c176dbd25270a0d99
Hannes Berger <xaaris@googlemail.com>
2016-02-21 16:13:25 +0100

    moved the example code to chooseTopicVC where it belongs

--- TFG/TFG/ChooseTopicViewController.swift
+++ TFG/TFG/ChooseTopicViewController.swift
@@ -91,5 +91,98 @@ class ChooseTopicViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func addTopicButtonPressed(sender: AnyObject) {
+        loadExampleData()
+    }
+    
+    func loadExampleData() {
+        if let keyedRowsDic = loadDataFromFile("Sample1"){
+            parseDataForValues(keyedRowsDic)
+        }else{
+            print("Could not load Data")
+        }
+        
+    }
+    
+    func loadDataFromFile(fileName: String) -> [[String : String]]? {
+        let csvURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(fileName, ofType: "csv")!)
+        do{
+            let text = try String(contentsOfURL: csvURL)
+            let csvImporter = CSVImporter(String: text, separator: ";")
+            if let keyedRows = csvImporter.keyedRows{
+                return keyedRows
+            }
+        }catch{
+            print(error)
+        }
+        return nil
+    }
+    
+    func parseDataForValues(keyedRowsDic: [[String:String]]) {
+        var title = ""
+        var author = ""
+        var date = ""
+        var universalTags = Set<String>()
+        for row in keyedRowsDic{
+            //Info row
+            if row["QuestionType"] == "Info"{
+                title = row["Question"]!.isEmpty ? "No title" : row["Question"]!
+                author = row["Hint"]!.isEmpty ? "No author" : row["Hint"]!
+                date = row["Feedback"]!.isEmpty ? "No Date" : row["Feedback"]!
+                for tag in row.values{
+                    if !tag.isEmpty{
+                        universalTags.insert(tag)
+                    }
+                }
+                universalTags.remove(title)
+                universalTags.remove(author)
+                universalTags.remove(date)
+                universalTags.remove("Info")
+                //Normal row
+            }else if !row["QuestionType"]!.isEmpty {
+                if let type = row["QuestionType"], question = row["Question"], hint = row["Hint"], feedback = row["Feedback"], difficulty = row["Difficulty"], correctAnswers = row["CorrectAnswers"]?.componentsSeparatedByString(","){
+                    var tmpAnswerDic = [String:String]()
+                    var answers = [String: Bool]()
+                    var tags = universalTags
+                    //extracting answers and tags
+                    for cell in row{
+                        if cell.0.containsString("Answer") && !cell.0.containsString("C") && !cell.1.isEmpty{
+                            tmpAnswerDic[cell.0.lowercaseString.stringByReplacingOccurrencesOfString("answer", withString: "")] = cell.1
+                        }else if cell.0.containsString("Tag") && !cell.1.isEmpty{
+                            tags.insert(cell.1)
+                        }
+                    }
+                    //mapping answers with their value of truth
+                    for answerKey in tmpAnswerDic.keys {
+                        if correctAnswers.contains(answerKey){
+                            answers[tmpAnswerDic[answerKey]!] = true
+                        }else{
+                            answers[tmpAnswerDic[answerKey]!] = false
+                        }
+                    }
+                    
+                    print("Type: \(type)")
+                    print("question: \(question)")
+                    print("Hint: \(hint)")
+                    print("Feedback: \(feedback)")
+                    print("Difficulty: \(difficulty)")
+                    print("CorrectAnswers: \(correctAnswers)")
+                    print("Answers: \(answers)")
+                    print("Tags: \(tags)")
+                    print("")
+                    
+                    
+                }else{
+                    print("Unwrapping type, Question, Hint, Feedback or CorrectAnswers failed!")
+                }
+            }
+        }
+        print("Title: \(title)")
+        print("Author: \(author)")
+        print("Date: \(date)")
+        print("Tags: \(universalTags)")
+        
+    }
 
 }
