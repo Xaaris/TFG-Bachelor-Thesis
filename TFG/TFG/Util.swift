356a35804808860b74159a77bbc2495846d43f40
Hannes Berger <xaaris@googlemail.com>
2016-02-26 03:25:13 +0100

    added a util class

---
+++ TFG/TFG/Util.swift
@@ -0,0 +1,28 @@
+//
+//  Util.swift
+//  TFG
+//
+//  Created by Johannes Berger on 26.02.16.
+//  Copyright © 2016 Johannes Berger. All rights reserved.
+//
+
+import Foundation
+
+class Util {
+    
+    func getCurrentTopic() -> Topic? {
+        let topics = realm.objects(Topic.self)
+        for topic in topics{
+            if topic.isSelected {
+                return topic
+            }
+        }
+        return nil
+    }
+    
+    
+    
+    
+    
+    
+}
