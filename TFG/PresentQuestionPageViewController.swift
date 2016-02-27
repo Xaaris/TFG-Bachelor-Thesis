c936d1841e91b6f4450f298db1784c12a49c1c4b
Hannes Berger <xaaris@googlemail.com>
2016-02-27 18:55:22 +0100

    build a custom page indicator but is not fully working yet

--- TFG/PresentQuestionPageViewController.swift
+++ TFG/PresentQuestionPageViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PresentQuestionPageViewController: UIPageViewController, UIPageViewControllerDataSource {
     
+    var pageControl:UIPageControl! = UIPageControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,21 +20,28 @@ class PresentQuestionPageViewController: UIPageViewController, UIPageViewControl
     }
     
     private func createPageViewController() {
-        
         self.dataSource = self
         let firstController = getPageController(0)!
         let startingViewControllers: NSArray = [firstController]
-        self.setViewControllers(startingViewControllers as? [UIViewController], direction: .Forward, animated: false, completion: nil)
-    
+        self.setViewControllers(startingViewControllers as? [UIViewController], direction: .Forward, animated: true, completion: nil)
     }
     
     private func setupPageControl() {
-        let appearance = UIPageControl.appearance()
-        appearance.pageIndicatorTintColor = UIColor.grayColor()
-        appearance.currentPageIndicatorTintColor = UIColor.darkGrayColor()
-        appearance.backgroundColor = UIColor.whiteColor()
+        pageControl = UIPageControl()
+        pageControl.frame = CGRectMake(0,60,self.view.frame.width,37)
+        self.view.addSubview(pageControl)
+        pageControl.pageIndicatorTintColor = UIColor.grayColor()
+        pageControl.currentPageIndicatorTintColor = UIColor.darkGrayColor()
+        pageControl.backgroundColor = UIColor.clearColor()
+        pageControl.numberOfPages = Util().getCurrentTopic()!.questions.count + 1
+        pageControl.currentPage = 0
     }
     
+    
+    //TODO: Not working yet as intended
+    func updatePageController(pageIndex: Int){
+        pageControl.currentPage = pageIndex
+    }
     
     
     
@@ -68,31 +76,20 @@ class PresentQuestionPageViewController: UIPageViewController, UIPageViewControl
     }
     
     private func getPageController(pageIndex: Int) -> UIViewController? {
-        
+        print(pageIndex)
+        updatePageController(pageIndex)
         if pageIndex < Util().getCurrentTopic()!.questions.count {
             let questionContentVC = self.storyboard!.instantiateViewControllerWithIdentifier("ContentController") as! QuestionContentViewController
             questionContentVC.pageIndex = pageIndex
             return questionContentVC
-        } else if pageIndex < Util().getCurrentTopic()!.questions.count + 1 {
+        } else if pageIndex == Util().getCurrentTopic()!.questions.count {
             let finishedVC = self.storyboard!.instantiateViewControllerWithIdentifier("QuizFinished") as! QuizFinishedViewController
             finishedVC.pageIndex = pageIndex
             return finishedVC
         }
-        
         return nil
     }
     
-    
-    
-    // MARK: - Page Indicator
-    
-    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return Util().getCurrentTopic()!.questions.count + 1
-    }
-    
-    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return 0
-    }
     
     //resets the questiondata so no question is answered and no answer is seleted
     func resetDataSet() {
@@ -108,5 +105,15 @@ class PresentQuestionPageViewController: UIPageViewController, UIPageViewControl
         }
         try! realm.commitWrite()
     }
-
+    
+    //Brings Pgae indicator to front
+    override func viewDidLayoutSubviews() {
+        for subView in self.view.subviews {
+            if subView is UIPageControl {
+                self.view.bringSubviewToFront(subView)
+            }
+        }
+        super.viewDidLayoutSubviews()
+    }
+    
 }
