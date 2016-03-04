462216315bb4ab9241d7a306b9705f5af1e8b225
Hannes Berger <xaaris@googlemail.com>
2016-03-04 02:00:47 +0100

    minor

--- TFG/TFG/CSVImporter.swift
+++ TFG/TFG/CSVImporter.swift
@@ -5,7 +5,7 @@
 //  Created by Daniel Haight on 30/08/2014.
 //  Copyright (c) 2014 ManyThings. All rights reserved.
 //
-//  Modified by Johannes Berger
+//  Modified and extended by Johannes Berger
 //
 
 import Foundation
