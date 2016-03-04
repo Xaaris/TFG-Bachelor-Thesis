9092d48551dfdd73e6197e5b5f686012fbf01c44
Hannes Berger <xaaris@googlemail.com>
2016-03-04 02:02:12 +0100

    added first switch to preferences

--- TFG/TFG/PreferencesViewController.swift
+++ TFG/TFG/PreferencesViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class PreferencesViewController: UIViewController {
     
+    
+    @IBOutlet weak var immediateFeedbackSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Preferences View")
+        if !realm.objects(Preference.self).isEmpty{
+            immediateFeedbackSwitch.on = realm.objects(Preference.self).first!.immediateFeedback
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,18 @@ class PreferencesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func immediateFeedbackSwitchPressed(sender: AnyObject) {
+        realm.beginWrite()
+        if let pref = realm.objects(Preference.self).first{
+            pref.immediateFeedback = !pref.immediateFeedback
+            realm.add(pref)
+        }else{
+            let pref = Preference()
+            pref.immediateFeedback = immediateFeedbackSwitch.on
+            realm.add(pref)
+        }
+        try! realm.commitWrite()
+    }
     
 }
 
