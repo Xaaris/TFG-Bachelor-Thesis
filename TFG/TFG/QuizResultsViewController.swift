3453fa1a242494ff6fa00af09e5f9e114421065f
Hannes Berger <xaaris@googlemail.com>
2016-03-10 05:31:59 +0100

    kinda broke it.. but i think im on to something

--- TFG/TFG/QuizResultsViewController.swift
+++ TFG/TFG/QuizResultsViewController.swift
@@ -64,14 +64,43 @@ class QuizResultsViewController: UIViewController{
         questionTextLabel.text = question.questionText
         questionTextLabel.font = UIFont.boldSystemFontOfSize(17.0)
         
-        let questionTitleStack = UIStackView()
-        questionTitleStack.axis = .Horizontal
-        questionTitleStack.distribution = .EqualSpacing
-        questionTitleStack.alignment = .Center
-        questionTitleStack.addArrangedSubview(expansionButton)
-        questionTitleStack.addArrangedSubview(questionTextLabel)
         
-        stack.addArrangedSubview(questionTitleStack)
+        
+        
+//        let questionTitleStack = UIStackView()
+//        questionTitleStack.axis = .Horizontal
+//        questionTitleStack.distribution = .EqualSpacing
+//        questionTitleStack.alignment = .Center
+//        questionTitleStack.addArrangedSubview(expansionButton)
+//        questionTitleStack.addArrangedSubview(questionTextLabel)
+//        questionTitleStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        let titleView = UIView()
+        titleView.addSubview(expansionButton)
+        titleView.addSubview(questionTextLabel)
+        titleView.heightAnchor.constraintEqualToConstant(64).active = true
+        titleView.widthAnchor.constraintEqualToConstant(350).active = true
+        titleView.backgroundColor = UIColor.redColor()
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        print(questionTextLabel.intrinsicContentSize().width)
+        print(questionTextLabel.intrinsicContentSize().height)
+        //Button Constraints
+        var leftSideConstraint = NSLayoutConstraint(item: expansionButton, attribute: .Left, relatedBy: .Equal, toItem: titleView, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        var bottomConstraint = NSLayoutConstraint(item: expansionButton, attribute: .CenterY , relatedBy: .Equal, toItem: titleView, attribute: .CenterY, multiplier: 1.0, constant: 0.0)
+//        var widthConstraint = NSLayoutConstraint(item: expansionButton, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 32)
+//        var heightConstraint = NSLayoutConstraint(item: expansionButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 32)
+        titleView.addConstraints([leftSideConstraint, bottomConstraint]) //, heightConstraint, widthConstraint])
+        
+        //questionTextLabel Constraints
+         leftSideConstraint = NSLayoutConstraint(item: questionTextLabel, attribute: .Left, relatedBy: .Equal, toItem: expansionButton, attribute: .Right, multiplier: 1.0, constant: 5)
+         bottomConstraint = NSLayoutConstraint(item: questionTextLabel, attribute: .CenterY, relatedBy: .Equal, toItem: expansionButton, attribute: .CenterY, multiplier: 1.0, constant: 0.0)
+//         widthConstraint = NSLayoutConstraint(item: questionTextLabel, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: questionTextLabel.intrinsicContentSize().width)
+//         heightConstraint = NSLayoutConstraint(item: questionTextLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: questionTextLabel.intrinsicContentSize().height)
+        titleView.addConstraints([leftSideConstraint, bottomConstraint]) //, heightConstraint, widthConstraint])
+        
+        
+        stack.addArrangedSubview(titleView)
         
         for answer in question.answers{
             let subStack = UIStackView()
