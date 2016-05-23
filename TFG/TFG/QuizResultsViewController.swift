2c4d8cc4924aa395e57f892504f616454133aa4e
Hannes Berger <xaaris@googlemail.com>
2016-05-23 16:33:17 +0200

    now bold for real

--- TFG/TFG/QuizResultsViewController.swift
+++ TFG/TFG/QuizResultsViewController.swift
@@ -212,7 +212,7 @@ class QuizResultsViewController: UIViewController{
         answerLabel.text = answer.answerText
         answerLabel.numberOfLines = 0
         if answer.isCorrect{
-            answerLabel.font = UIFont.bold
+            answerLabel.font = UIFont.boldSystemFontOfSize(17.0)
             answerLabel.textColor = Util.myGreenColor
         }else{
             answerLabel.textColor = Util.myRedColor
