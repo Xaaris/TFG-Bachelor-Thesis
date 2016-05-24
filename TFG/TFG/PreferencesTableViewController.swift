f6a610fa14542ee5edbb6e64c07ae1901490801f
Hannes Berger <xaaris@googlemail.com>
2016-05-24 20:39:32 +0200

    Build new preferences screen

--- TFG/TFG/PreferencesTableViewController.swift
+++ TFG/TFG/PreferencesTableViewController.swift
@@ -1,47 +1,93 @@
 //
-//  SecondViewController.swift
+//  PreferencesTableViewController.swift
 //  TFG
 //
-//  Created by Johannes Berger on 14.02.16.
+//  Created by Johannes Berger on 23.05.16.
 //  Copyright © 2016 Johannes Berger. All rights reserved.
 //
 
 import UIKit
 import Parse
 
-class PreferencesViewController: UIViewController {
+class PreferencesTableViewController: UITableViewController {
     
+    @IBOutlet var prefTableView: UITableView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var feedbackSwitch: UISwitch!
+    @IBOutlet weak var lockSwitch: UISwitch!
+    @IBOutlet weak var secondsSlider: UISlider!
     
-    @IBOutlet weak var immediateFeedbackSwitch: UISwitch!
-    @IBOutlet weak var secondsBeforeLockLabel: UILabel!
-    @IBOutlet weak var secondsLabel: UILabel!
-    @IBOutlet weak var lockSecondsSlider: UISlider!
+    @IBOutlet weak var lockTimerCell: UITableViewCell!
+    @IBOutlet weak var sliderCell: UITableViewCell!
+    
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var pref: Preference!
     
-    ///Prepare the view with the current preference settings
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Preferences View")
+        
         pref = Util.getPreferences()!
-        immediateFeedbackSwitch.on = pref.immediateFeedback
-        enableLockSecondsSlider(pref.immediateFeedback)
-        lockSecondsSlider.value = Float(pref.lockSeconds)
-        secondsLabel.text = String(pref.lockSeconds)
+        prepareView()
+    }
+
+    
+    func prepareView(){
+        userNameLabel.text = Util.getCurrentUserName()
+        feedbackSwitch.on = pref.immediateFeedback
+        secondsSlider.value = Float(pref.lockSeconds)
+        prefTableView.reloadData()
     }
     
+    // MARK: - Table view data source
+    
+    ///Returns the number of rows per section. Here used to hide rows when the are not needed
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            if pref.immediateFeedback{
+                return 3
+            }else{
+                return 1
+            }
+        case 2:
+            return 2
+        default:
+            return 1
+        }
+    }
+ 
+    ///Returns the footers for each section. Here used to hide a footer when its not needed
+    override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch section {
+        case 1:
+            if pref.immediateFeedback{
+                return NSLocalizedString("Seconds before a question automatically locks", comment: "Footer that gets displayed under the slider for the lock seconds")
+            }else{
+                return nil
+            }
+        default:
+            return nil
+        }
+    }
+
     /**
-    Toggle immediate feedback switch and save result locally and to cloud (async)
+     Toggle immediate feedback switch and save result locally and to cloud (async)
      - parameter sender: user interaction
-    */
-    @IBAction func immediateFeedbackSwitchPressed(sender: AnyObject) {
+     */
+    @IBAction func feedbackSwitchtoggled(sender: AnyObject) {
         realm.beginWrite()
         pref.immediateFeedback = !pref.immediateFeedback
         realm.add(pref)
         try! realm.commitWrite()
-        enableLockSecondsSlider(pref.immediateFeedback)
+        prefTableView.reloadData()
         CloudLink.syncPreferencesToCloud()
+    }
+    
+    @IBAction func lockSwitchToggled(sender: AnyObject) {
     }
     
     /**
@@ -49,15 +95,14 @@ class PreferencesViewController: UIViewController {
      the value if it differs from the old one.
      - parameter sender: user interaction
      */
-    @IBAction func lockSecondsSliderValueDidChange(sender: AnyObject) {
+    @IBAction func secondsSliderMoved(sender: AnyObject) {
         let oldValue = pref.lockSeconds
-        let newValue = Int(lroundf(lockSecondsSlider.value))
+        let newValue = Int(lroundf(secondsSlider.value))
         //Fix slider to integer position
-        sender.setValue(Float(lroundf(lockSecondsSlider.value)), animated: true)
+        sender.setValue(Float(lroundf(secondsSlider.value)), animated: true)
         if oldValue != newValue{
-            secondsLabel.text = String(Int(lockSecondsSlider.value))
             realm.beginWrite()
-            pref.lockSeconds = Int(lockSecondsSlider.value)
+            pref.lockSeconds = Int(secondsSlider.value)
             realm.add(pref)
             try! realm.commitWrite()
             CloudLink.syncPreferencesToCloud()
@@ -65,21 +110,10 @@ class PreferencesViewController: UIViewController {
     }
     
     /**
-     Enables/disables the slider and its labels
-     - parameter enable: Bool to determine if it should be enabled or disabled
-     */
-    func enableLockSecondsSlider(enable: Bool){
-        secondsBeforeLockLabel.enabled = enable
-        secondsLabel.enabled = enable
-        lockSecondsSlider.enabled = enable
-    }
-    
-    /**
      Brings up an alertview asking the user if he/she wants to delete the statistics
      - parameter sender: user interaction
      */
     @IBAction func deleteStatisticsButtonPressed(sender: AnyObject) {
-        
         let title = NSLocalizedString("Delete Statistics?", comment: "")
         let message = NSLocalizedString("Are you sure you want to delete all statistics? This cannot be undone!", comment: "")
         let delete = NSLocalizedString("Delete", comment: "")
@@ -95,13 +129,11 @@ class PreferencesViewController: UIViewController {
         self.presentViewController(alertController, animated: true, completion: nil)
     }
     
-
     /**
      Brings up an alertview asking the user if he/she wants to log out
      - parameter sender: user interaction
      */
-    @IBAction func logOutButtonPressed(sender: AnyObject) {
-        
+    @IBAction func LogOutButtonPressed(sender: AnyObject) {
         let title = NSLocalizedString("Log Out?", comment: "")
         let message = NSLocalizedString("Are you sure you want to log out?", comment: "")
         let logOut = NSLocalizedString("Log Out", comment: "")
@@ -150,13 +182,14 @@ class PreferencesViewController: UIViewController {
     /**
      Shows an overlay alert with an "OK" button to dimiss it
      - parameters:
-        - title: Title of the alert
-        - message: body of the alert
+     - title: Title of the alert
+     - message: body of the alert
      */
     func showAlert(title: String, message: String) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
         self.presentViewController(alertController, animated: true, completion: nil)
     }
-}
 
+
+}
